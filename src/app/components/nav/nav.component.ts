d72cf61cf9fdd89f0a595a8d7f7667ac214349a0
Théo Nogueira <theonnogueira@outlook.com>
2023-10-18 13:55:46 -0300

    HTML e CSS para componente home

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"],
 })
 export class NavComponent implements OnInit {
-
-  constructor( private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.router.navigate(['home'])
+    this.router.navigate(["home"]);
   }
-
 }
